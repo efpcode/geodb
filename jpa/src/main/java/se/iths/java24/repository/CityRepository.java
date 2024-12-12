469825575e6ec20ecdcb2f3931e5da3f802da962
Martin Blomberg <martin.blomberg@outlook.com>
2024-12-12 14:28:28 +0100

    Add method to CityRepository
    Adds a method that takes a minpopulation and returns a List of CititesWithHighPopulation containing cityName and population if the city has a higher population than minpopulation.

--- jpa/src/main/java/se/iths/java24/repository/CityRepository.java
+++ jpa/src/main/java/se/iths/java24/repository/CityRepository.java
@@ -3,9 +3,21 @@ package se.iths.java24.repository;
 import se.iths.java24.JPAUtil;
 import se.iths.java24.entity.City;
 
+import java.util.List;
+
+import static se.iths.java24.JPAUtil.inTransaction;
+
 public class CityRepository {
 
     public void saveCity(City city) {
-        JPAUtil.inTransaction(em -> em.persist(city));
+        inTransaction(em -> em.persist(city));
+    }
+
+    public List<City> allCitiesWithMoreThan(int minpopulation) {
+        return JPAUtil.getEntityManager()
+                .createQuery("select c from City c where c.population > :minpopulation",
+                        City.class)
+                .setParameter("minpopulation", minpopulation)
+                .getResultList();
     }
 }
