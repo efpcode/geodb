e51919fe0a1b90649a1a9d47c198cadf8d1e4f8e
Aretha Pierre <aretha.pierre@iths.se>
2024-12-18 01:04:31 +0100

    Add the Repository implementation.

--- jpa/src/main/java/geodb/repository/CityRepository.java
+++ jpa/src/main/java/geodb/repository/CityRepository.java
@@ -1,28 +1,62 @@
 package geodb.repository;
 
+import geodb.JPAUtil;
 import geodb.entity.City;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class CityRepository implements Repository<City, Integer> {
 
     @Override
     public City save(City city) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return CityRepository.inTransactionWithResult(entityManager -> {
+            if (city.getId() == null) {
+                entityManager.persist(city);
+            } else {
+                entityManager.merge(city);
+            }
+            return city;
+        });
     }
 
     @Override
     public List<City> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return CityRepository.inTransactionWithResult(entityManager ->
+                entityManager.createQuery("SELECT c FROM City c", City.class).getResultList());
     }
 
     @Override
     public City findById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return CityRepository.inTransactionWithResult(entityManager -> entityManager.find(City.class, id));
     }
 
     @Override
     public void deleteById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        JPAUtil.inTransaction(entityManager -> {
+            City city = entityManager.find(City.class, id);
+            if (city != null) {
+                entityManager.remove(city);
+            }
+        });
+    }
+
+    private static <T> T inTransactionWithResult(Function<EntityManager, T> function) {
+        EntityTransaction transaction;
+        try (EntityManager em = JPAUtil.getEntityManager()) {
+            transaction = em.getTransaction();
+            try {
+                transaction.begin();
+                T result = function.apply(em);
+                transaction.commit();
+                return result;
+            } finally {
+                if (transaction.isActive()) {
+                    transaction.rollback();
+                }
+            }
+        }
     }
 }
