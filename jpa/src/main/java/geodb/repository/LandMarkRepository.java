b4e0c816d488b7fb956ffbf63306feb6c0f2f520
grubl <vilhelm.kennedal@iths.se>
2024-12-18 09:52:31 +0100

    add repository class, import scanner

---
+++ jpa/src/main/java/geodb/repository/LandMarkRepository.java
@@ -0,0 +1,30 @@
+package geodb.repository;
+
+import geodb.Crudable;
+
+import java.util.Scanner;
+
+public class LandMarkRepository implements Crudable {
+
+    private Scanner sc = new Scanner(System.in);
+
+    @Override
+    public void insertToTable() {
+
+    }
+
+    @Override
+    public void updateTable() {
+
+    }
+
+    @Override
+    public void deleteRowInTable() {
+
+    }
+
+    @Override
+    public void displayTable() {
+
+    }
+}
