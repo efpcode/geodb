116a7ffbec922a0380f01c541f599789aed481ed
alexa <alexander.wettergren.stenders@iths.se>
2024-12-18 14:49:28 +0100

    Updated updateTable method so OceanID input can't be lower or equal to 0

--- jpa/src/main/java/geodb/repository/OceanRepository.java
+++ jpa/src/main/java/geodb/repository/OceanRepository.java
@@ -33,8 +33,8 @@ public class OceanRepository implements Crudable {
         int updateTableStringOceanID = scanner.nextInt();
         String updateTableStringOcean = scanner.nextLine();
 
-        if(updateTableStringOcean.isEmpty()) {
-            System.out.println("Empty input");
+        if(updateTableStringOcean.isEmpty() || updateTableStringOceanID <= 0) {
+            System.out.println("Empty/Invalid input");
             return;
         }
 
