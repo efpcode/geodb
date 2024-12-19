4b305c7dd5dd313e412727b39eb221fdc02409fb
Aretha Pierre <aretha.pierre@iths.se>
2024-12-19 10:09:34 +0100

    Fix before merge

--- jpa/src/main/java/geodb/entity/City.java
+++ jpa/src/main/java/geodb/entity/City.java
@@ -68,7 +68,7 @@ public class City {
         this.cityArea = cityArea;
     }
 
-    public Boolean isCityCapital() {
+    public Boolean getCityCapital() {
         return cityCapital;
     }
 
--- jpa/src/main/java/geodb/repository/CityRepository.java
+++ jpa/src/main/java/geodb/repository/CityRepository.java
@@ -1,62 +1,164 @@
 package geodb.repository;
 
-import geodb.JPAUtil;
+import geodb.Crudable;
 import geodb.entity.City;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 
+import java.util.InputMismatchException;
 import java.util.List;
-import java.util.function.Function;
+import java.util.Scanner;
 
-public class CityRepository implements Repository<City, Integer> {
+import static geodb.JPAUtil.inTransaction;
+
+public class CityRepository implements Crudable {
+    private static Scanner scanner = new Scanner(System.in);
 
     @Override
-    public City save(City city) {
-        return CityRepository.inTransactionWithResult(entityManager -> {
-            if (city.getId() == null) {
-                entityManager.persist(city);
-            } else {
-                entityManager.merge(city);
-            }
-            return city;
-        });
+    public void insertToTable() {
+        City city = new City();
+        System.out.println("Enter the name of the city: ");
+        String cityName = scanner.nextLine();
+
+        if (cityName.isEmpty()) {
+            throw new RuntimeException("City name cannot be empty");
+        }
+
+        System.out.println("Enter the population of the city: ");
+        long population = scanner.nextLong();
+        scanner.nextLine();
+
+        if (population <= 0) {
+            throw new RuntimeException("Population must be larger than 0");
+        }
+
+        System.out.println("Enter the area of the city: ");
+        double area = scanner.nextDouble();
+        scanner.nextLine();
+
+        if (area <= 0) {
+            throw new RuntimeException("Area must be larger than 0");
+        }
+        System.out.println("Is this a capital (Y/N)? ");
+        String capital = scanner.nextLine();
+
+        boolean isCapital = capital.equals("Y");
+
+        city.setCityName(cityName);
+        city.setCityPopulationSize(population);
+        city.setCityArea(area);
+        city.setCityCapital(isCapital);
+        inputInsertToTable(city);
+    }
+
+    private static void inputInsertToTable(City city) {
+        inTransaction(entityManager -> entityManager.persist(city));
     }
 
     @Override
-    public List<City> findAll() {
-        return CityRepository.inTransactionWithResult(entityManager ->
-                entityManager.createQuery("SELECT c FROM City c", City.class).getResultList());
+    public void updateTable() {
+
+        System.out.println("Enter the id of the city you want to update: ");
+        int cityId = scanner.nextInt();
+        scanner.nextLine();
+        if (cityId <= 0) {
+            throw new RuntimeException("Id must be larger than 0");
+        }
+
+        System.out.println("Enter the name of the city: ");
+        String cityName = scanner.nextLine();
+        validateCityName(cityName);
+
+        System.out.println("Enter the population of the city: ");
+        long population = scanner.nextLong();
+        scanner.nextLine();
+        validateCityPopulation(population);
+
+        System.out.println("Enter the area of the city: ");
+        double area = scanner.nextDouble();
+        scanner.nextLine();
+        validateCityArea(area);
+
+        System.out.println("Enter it it's a capital(Y/N): ");
+        String isCapitalInput = scanner.nextLine();
+        validateIsCapital(isCapitalInput);
+        boolean isCapital = isCapitalInput.equals("Y");
+
+
+        inputUpdateTable(cityId, cityName, population, area, isCapital);
     }
 
     @Override
-    public City findById(Integer id) {
-        return CityRepository.inTransactionWithResult(entityManager -> entityManager.find(City.class, id));
-    }
+    public void deleteRowInTable() {
 
-    @Override
-    public void deleteById(Integer id) {
-        JPAUtil.inTransaction(entityManager -> {
-            City city = entityManager.find(City.class, id);
+        System.out.println("Enter city id to delete: ");
+        int cityID = scanner.nextInt();
+        scanner.nextLine();
+
+        if (cityID <= 0) {
+            throw new RuntimeException("ID must be a positive number greater than 0");
+        }
+
+        inTransaction(entityManager -> {
+            City city = entityManager.find(City.class, cityID);
             if (city != null) {
                 entityManager.remove(city);
             }
         });
     }
 
-    private static <T> T inTransactionWithResult(Function<EntityManager, T> function) {
-        EntityTransaction transaction;
-        try (EntityManager em = JPAUtil.getEntityManager()) {
-            transaction = em.getTransaction();
-            try {
-                transaction.begin();
-                T result = function.apply(em);
-                transaction.commit();
-                return result;
-            } finally {
-                if (transaction.isActive()) {
-                    transaction.rollback();
-                }
-            }
+    @Override
+    public void displayTable() {
+        System.out.println("City Table\n\n");
+        System.out.println("City ID | City name | City population | City area | City capital");
+        inTransaction(entityManager -> {
+            TypedQuery<City> query = entityManager.createQuery("SELECT c FROM City c", City.class);
+            List<City> rows = query.getResultList();
+            rows.stream()
+                    .map(n -> n.getId() + ".  |  " + n.getCityName() + " | " + n.getCityPopulationSize() + " | " + n.getCityArea() + " | " + n.getCityCapital())
+                    .forEach(System.out::println);
+        });
+    }
+
+    private void validateCityArea(double area) {
+        if (area < 0) {
+            throw new RuntimeException("Area must not be negative");
         }
     }
+
+    private void validateCityPopulation(long population) {
+        if (population < 0) {
+            throw new RuntimeException("Population must be not be negative");
+        }
+    }
+
+    private void validateCityName(String cityName) {
+        if (cityName.trim().isEmpty()) {
+            throw new RuntimeException("City name cannot be empty");
+        } else if (cityName.matches(".*\\d.*")) {
+            throw new InputMismatchException("City name must not contain numbers");
+        }
+    }
+
+    private void validateIsCapital(String isCapitalInput) {
+        if (isCapitalInput.trim().isEmpty()) {
+            throw new RuntimeException("Answer cannot be empty");
+        } else if (!isCapitalInput.matches("^[yYnN]+$")) {
+            throw new RuntimeException("Not a valid answer (Y/N)");
+        }
+    }
+
+    private static void inputUpdateTable(int newCityId, String newCityName, long newPopulation, double newArea, boolean isCapital) {
+        inTransaction(entityManager -> {
+            City newCity = entityManager.find(City.class, newCityId);
+            if (newCity != null) {
+                newCity.setCityName(newCityName);
+                newCity.setCityPopulationSize(newPopulation);
+                newCity.setCityArea(newArea);
+                newCity.setCityCapital(isCapital);
+            }
+        });
+    }
 }
+
+
+
