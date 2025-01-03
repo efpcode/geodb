5d28e2d0f7ab19b63db07f736bbda2aaaa3df171
efpcode <efpjobhunt@gmail.com>
2025-01-03 12:43:11 +0100

    Adds custom exception for invalid option picked.

--- jpa/src/main/java/geodb/RootView.java
+++ jpa/src/main/java/geodb/RootView.java
@@ -1,4 +1,5 @@
 package geodb;
+import geodb.ViewNotImplementedYet.*;
 
 public class RootView implements Viewable {
     @Override
@@ -23,6 +24,28 @@ public class RootView implements Viewable {
 
     @Override
     public void promptView() {
+        String banner = """
+                 __          __         _                                       _                _____                  _____    ____ \s
+                 \\ \\        / /        | |                                     | |              / ____|                |  __ \\  |  _ \\\s
+                  \\ \\  /\\  / /    ___  | |   ___    ___    _ __ ___     ___    | |_    ___     | |  __    ___    ___   | |  | | | |_) |
+                   \\ \\/  \\/ /    / _ \\ | |  / __|  / _ \\  | '_ ` _ \\   / _ \\   | __|  / _ \\    | | |_ |  / _ \\  / _ \\  | |  | | |  _ <\s
+                    \\  /\\  /    |  __/ | | | (__  | (_) | | | | | | | |  __/   | |_  | (_) |   | |__| | |  __/ | (_) | | |__| | | |_) |
+                     \\/  \\/      \\___| |_|  \\___|  \\___/  |_| |_| |_|  \\___|    \\__|  \\___/     \\_____|  \\___|  \\___/  |_____/  |____/\s
+                """;
+        System.out.println(banner);
 
+
+
+    }
+
+    public void optionsAvailable(){
+
+
+
+    }
+
+    public static void main(String[] args) {
+        RootView v = new RootView();
+        v.promptView();
     }
 }
